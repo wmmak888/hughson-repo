b7315692a98b7111feeb497913f4c85541d98f0f
Mark Mak <83636992+wmmak888@users.noreply.github.com>
2021-06-15 11:57:10 +0800

    Update header.js

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -6,14 +6,11 @@ import {cn} from '../lib/helpers'
 import styles from './header.module.css'
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
-              <div style={{backgroundcolor:'#334CCB', overflow:'hidden' }} >
-                  <Link to='/'>Home</Link>
-                        <div style={{float:'right'}} >
-                          <Link to='#hero'>About</Link>
+                        <div style={{background:'#334CCB',float:'right'}} >
+                          <Link to='/'>About</Link>
                           <Link to='/project/'>Project</Link>
                           <Link to='/blog/'>Blog</Link>
                         </div>
-               </div>
 )
 
 export default Header
