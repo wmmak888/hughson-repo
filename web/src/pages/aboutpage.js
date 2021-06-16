ce517952cbfc09489d8d0ff79c0e7c30edd863b8
Mark Mak <83636992+wmmak888@users.noreply.github.com>
2021-06-16 17:20:52 +0800

    Update aboutpage.js

--- web/src/pages/aboutpage.js
+++ web/src/pages/aboutpage.js
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  mapEdgesToNodes,
+  filterOutDocsWithoutSlugs,
+  filterOutDocsPublishedInTheFuture,
+} from "../lib/helpers";
+import Container from "../components/container";
 import SEO from "../components/seo";
 import Layout from "../containers/layout";
 import { AboutSection } from "../components/aboutSection";
@@ -6,7 +12,7 @@ import { AboutSection } from "../components/aboutSection";
 const aboutpage = () => (
   <Layout>
     <SEO title='About' />
-    <aboutSection />; 
+    <AboutSection />; 
   </Layout>
 )
 
