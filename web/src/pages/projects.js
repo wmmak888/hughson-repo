ed36897afe0f6430e8bacec57bcedc884860c08c
Mark Mak <83636992+wmmak888@users.noreply.github.com>
2021-06-16 14:36:59 +0800

    Update projects.js

--- web/src/pages/projects.js
+++ web/src/pages/projects.js
@@ -1,20 +1,7 @@
 import React from "react";
-import { graphql } from "gatsby";
-import {
-  mapEdgesToNodes,
-  filterOutDocsWithoutSlugs,
-  filterOutDocsPublishedInTheFuture,
-} from "../lib/helpers";
-import BlogPostPreviewList from "../components/blog-post-preview-list";
-import Container from "../components/container";
-import GraphQLErrorList from "../components/graphql-error-list";
 import SEO from "../components/seo";
 import Layout from "../containers/layout";
-
 import { ProjectsList } from "../components/projectsList";
-
-import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const projects = () => (
   <Layout>
