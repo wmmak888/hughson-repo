5e26e2ade29e52035330d3fcfd08ca792cded103
Mark Mak <83636992+wmmak888@users.noreply.github.com>
2021-06-16 15:48:29 +0800

    Update header.js

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -11,7 +11,7 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
          <div className={styles.branding}>
                         <div style={{color:'white', float:'right'}} >
                           <Link to='/'>Home</Link>
-                          <Link to='/about/'>About</Link>
+                          <Link to='/aboutpage/'>About</Link>
                           <Link to='/projects/'>Projects</Link>
                           <Link to='/blog/'>Blog</Link>
                         </div>
