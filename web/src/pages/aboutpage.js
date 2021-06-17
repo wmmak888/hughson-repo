bd960a1eba0817878083096bc173c04120f069c5
Mark Mak <83636992+wmmak888@users.noreply.github.com>
2021-06-17 11:48:02 +0800

    Update aboutpage.js

--- web/src/pages/aboutpage.js
+++ web/src/pages/aboutpage.js
@@ -1,13 +1,85 @@
 import React from "react";
+import { graphql } from "gatsby";
 import {
   mapEdgesToNodes,
   filterOutDocsWithoutSlugs,
   filterOutDocsPublishedInTheFuture,
 } from "../lib/helpers";
 import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
 import SEO from "../components/seo";
 import Layout from "../containers/layout";
 import { AboutSection } from "../components/aboutSection";
+
+
+export const query = graphql`
+  query MyQuery1 {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      description
+      keywords
+    }
+    page: sanityPage(path: { eq: "/" }) {
+      id
+      content {
+        ... on SanityAboutSection {
+          _key
+          _type
+          headline
+          experiences {
+            title
+            years
+          }
+          passions {
+            title
+            icon
+          }
+          skills {
+            title
+            icon
+          }
+          tools {
+            title
+            icon
+          }
+          schools {
+            title
+            years
+          }
+        }
+        ... on SanityBlogList {
+          _key
+          _type
+          headline
+        }
+        ... on SanityHero {
+          _key
+          _type
+          ctas {
+            title
+            link
+          }
+          description
+          headline
+          mainImage {
+            asset {
+              url
+              fluid {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+          topline
+        }
+        ... on SanityProjectList {
+          _key
+          _type
+          headline
+        }
+      }
+    }
+  }
+`;
 
 const aboutpage = () => (
   <Layout>
